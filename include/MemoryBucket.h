0512c07b92c7f6e645bd98939550d3a643decd8a
Pardal <marcosnascimento@eng.ci.ufpb.br>
2024-10-05 08:31:06 -0300

    add last used at memory bucket class

--- include/MemoryBucket.h
+++ include/MemoryBucket.h
@@ -1,17 +1,24 @@
 #ifndef MEMORY_BUCKET_H
 #define MEMORY_BUCKET_H
 
+#include <time.h>
+
 
 class MemoryBucket {
   private:
     int value;
+    time_t last_used;
 
     public:
       MemoryBucket(int value): value(value) {}
 
       int get_bucket_value();
 
+      time_t get_bucket_last_used();
+
       void set_bucket_value(int new_value);
+
+      void use_bucket();
 };
 
 #endif
--- src/MemoryBucket.cpp
+++ src/MemoryBucket.cpp
@@ -6,4 +6,13 @@ int MemoryBucket::get_bucket_value() {
 
 void MemoryBucket::set_bucket_value(int new_value) {
   this->value = new_value;
+  this->use_bucket();
+}
+
+void MemoryBucket::use_bucket() {
+  this->last_used = time(NULL);
+}
+
+time_t MemoryBucket::get_bucket_last_used() {
+  return this->last_used;
 }

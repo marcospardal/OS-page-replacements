4d641e77d55d48fdfaf76ed0b82fd6d951d9aa48
Pardal <marcosnascimento@eng.ci.ufpb.br>
2024-10-05 08:28:51 -0300

    fix create item FIFO

--- src/FIFO.cpp
+++ src/FIFO.cpp
@@ -5,6 +5,5 @@ void FIFO::replace_page(int page) {
 
   memory.erase(memory.begin());
 
-  MemoryBucket new_element = MemoryBucket(page);
-  memory.push_back(new_element);
+  this->create_item(page);
 }
